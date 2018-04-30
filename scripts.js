b247978d076c5ba138c6df4bdaf034def501d02a
Agnieszka Szczepkowska <agnieszka.szczepkowska.work@gmail.com>
2018-04-30 13:34:20 +0200

    first commit

---
+++ scripts.js
@@ -0,0 +1,9 @@
+var a = 2;
+var b = 4;
+var value = (a * a) - (2 * a * b) - (b * b);
+
+console.log('Wynik działania (a * a) - (2 * a * b) - (b * b) to:' + value);
+
+console.log((value > 0) ? 'Wynik dodatni' : 'Wynik ujemny');
+
+console.log((value == 0) ? 'Wynik jest rowny 0' : 'Wynik nie jest rowny 0');
